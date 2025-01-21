62adb66c0d77c0084851278ad36f41c13ca621e8
amantes30 <amanueltesfaye55@gmail.com>
2025-01-21 18:19:53 +0800

    add feature to make sure the form is focused on

--- app.js
+++ app.js
@@ -1,44 +1,81 @@
+let isTerminalActive = true;
+const terminalFrom = document.getElementById('terminal-form');
+
+// make sure the terminal is focused on
+terminalFrom.addEventListener('click', ()=>{
+    terminalFrom.classList.toggle('active', true);
+    isTerminalActive = true;
+})
+document.addEventListener('mousedown', (evt)=>{
+    if(evt.target.id !== 'terminal-form'){
+        terminalFrom.classList.toggle('active', false);
+        isTerminalActive = false;
+    }
+})
+
+document.addEventListener('keypress', function(event) {
+    if(isTerminalActive) {
+        const text = document.getElementById('active');
+        if (event.key === "Enter") {
+            text.id="";
+            // remove blink underscore
+            text.parentElement.children[1].innerHTML = "";
+            const command = text.textContent;
+            handleSubmit(command)
+            return
+        }
+        write(text, event.key);
+    }
+})// backspace
+document.addEventListener('keydown', function(event) {
+    if(isTerminalActive) {
+        const text = document.getElementById('active');
+        if (event.key === "Backspace") {
+            text.textContent = text.textContent.slice(0,-1);
+        }
+    }
+})
+
+
+
+// Functions
 function handleSubmit(input){
-    const terminalFrom = document.getElementById('terminal-form');
     const output = document.createElement('h2');
     output.textContent = input;
     const newline = document.createElement('h1');
     switch(input) {
         case 'cls':
             terminalFrom.innerHTML = "";
-            terminalFrom.innerHTML = `<h1>~/amantes30><span id="active" class="text"></span><span class="blink">_</span></h1>`;
+            break;
+        case 'projects':
+            output.innerHTML = `<a href="#">Photos</a>`
+            terminalFrom.appendChild(output);
+            break;
+        case 'help':
+            output.innerHTML = `
+            <h3>Commands</h3>
+            <p>cls - <span>Clears the terminal</span></p>
+            <p>help - <span>List available commands</span></p>
+            `;
+            terminalFrom.appendChild(output);
             break;
         default:
             output.textContent = `Invalid command: "${input}"`;
             output.classList.add('error');
             terminalFrom.appendChild(output);
-            newline.innerHTML = `~/amantes30><span id="active" class="text"></span><span class="blink">_</span>`
-            terminalFrom.appendChild(newline);
-            terminalFrom.scrollTo(0, terminalFrom.scrollHeight)
             break;
     }
+    makeNewLine();
+    function makeNewLine(){
+        newline.innerHTML = `~/amantes30><span id="active" class="text"></span><span class="blink">_</span>`
+        terminalFrom.appendChild(newline);
+        terminalFrom.scrollTo(0, terminalFrom.scrollHeight)
+    }
 }
-document.addEventListener('keypress', function(event) {
-    const text = document.getElementById('active');
-    if (event.key === "Enter") {
-        text.id="";
-        // remove blink underscore
-        text.parentElement.children[1].innerHTML = "";
-        const command = text.textContent;
-        handleSubmit(command)
-        return
-    }
-
-    // write
-    let curr_text = text.textContent;
+// write
+function write(current_txt, letter){
+    let curr_text = current_txt.textContent;
     let temp = curr_text.split('')
-    temp[temp.length] = event.key;
-    text.textContent = temp.join('');
-})
-// backspace
-document.addEventListener('keydown', function(event) {
-    const text = document.getElementById('active');
-    if (event.key === "Backspace") {
-        text.textContent = text.textContent.slice(0,-1);
-    }
-})
+    temp[temp.length] = letter;
+    current_txt.textContent = temp.join('');
+}
